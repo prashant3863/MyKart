be5b498da2cfef9ea1da71a5d2f558a634c2b116
Chirag Aggarwal <me@chi6rag.net>
2015-12-14 20:08:49 +0530

    Adding ability to toggle navigation drawer

--- app/src/main/java/com/example/chi6rag/mykart/MainActivity.java
+++ app/src/main/java/com/example/chi6rag/mykart/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.chi6rag.mykart;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = "chi6rag";
+    private DrawerLayout mDrawerLayout;
+    private LinearLayout mNavigationDrawer;
+    private ListView mNavigationDrawerOptionsList;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
 
     public static String[] CATEGORIES = {
             "Men",
@@ -17,9 +27,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView navigationDrawerOptionsList = (ListView) findViewById(R.id.navigation_drawer_options);
-        NavigationDrawerListAdapter navigationDrawerListAdapter = new NavigationDrawerListAdapter(this);
-        navigationDrawerOptionsList.setAdapter(navigationDrawerListAdapter);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.main_activity_drawer_layout);
+        mNavigationDrawer = (LinearLayout) findViewById(R.id.navigation_drawer);
+        mNavigationDrawerOptionsList = (ListView) findViewById(R.id.navigation_drawer_options);
 
+        NavigationDrawerListAdapter navigationDrawerListAdapter = new NavigationDrawerListAdapter(this);
+        mNavigationDrawerOptionsList.setAdapter(navigationDrawerListAdapter);
+
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mActionBarDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(hasClickedNavigationDrawerIcon(item)) {
+            if(mDrawerLayout.isDrawerOpen(mNavigationDrawer)) {
+                mDrawerLayout.closeDrawer(mNavigationDrawer);
+            }
+            else {
+                mDrawerLayout.openDrawer(mNavigationDrawer);
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mActionBarDrawerToggle.syncState();
+    }
+
+    private boolean hasClickedNavigationDrawerIcon(MenuItem item) {
+        return mActionBarDrawerToggle.onOptionsItemSelected(item);
     }
 }
