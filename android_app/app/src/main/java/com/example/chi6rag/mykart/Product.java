33a0a9753494b499d8c91e299a0a9822d42fb5d0
Chirag Aggarwal <me@chi6rag.net>
2015-12-16 09:11:49 +0530

    Adding functionality to find the first image resource of a product and calculate formatted price

--- android_app/app/src/main/java/com/example/chi6rag/mykart/Product.java
+++ android_app/app/src/main/java/com/example/chi6rag/mykart/Product.java
@@ -1,6 +1,8 @@
 package com.example.chi6rag.mykart;
 
 public class Product {
+    private static final String RUPEES_SYMBOL = "Rs. ";
+    public static String ID_TAG = String.valueOf(R.id.product_id_tag);
     public String name;
     public String description;
     public double price;
@@ -15,5 +17,15 @@ public class Product {
         this.price = price;
         this.category = category;
         this.imageResources = imageResources;
+    }
+
+    public int firstImageResource() {
+        if(imageResources.length > 0)
+            return imageResources[0];
+        return 0;
+    }
+
+    public String formattedPrice() {
+        return RUPEES_SYMBOL + String.valueOf(this.price);
     }
 }
