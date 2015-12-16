5c8000cfd34601c1f96ca39a3378115a0fde609f
Chirag Aggarwal <me@chi6rag.net>
2015-12-16 08:11:14 +0530

    Changing contract of product

--- android_app/app/src/main/java/com/example/chi6rag/mykart/MainActivity.java
+++ android_app/app/src/main/java/com/example/chi6rag/mykart/MainActivity.java
@@ -30,17 +30,17 @@ public class MainActivity extends AppCompatActivity {
     };
 
     public static Product[] PRODUCTS = {
-            new Product("Skinny-Fit Big Pony Polo", "Ribbed polo collar\nTwo-button placket\nLong sleeves with ribbed cuffs", 14990, "Tops",
+            new Product(1, "Skinny-Fit Big Pony Polo", "Ribbed polo collar\nTwo-button placket\nLong sleeves with ribbed cuffs", 14990, "Tops",
                     new int[]{R.drawable.skinny_fit_big_pony_polo_1, R.drawable.skinny_fit_big_pony_polo_2}),
-            new Product("Tweed & Faux Leather Tank", "Ribbed polo collar\nTwo-button placket\nLong sleeves with ribbed cuffs", 14990, "Tops",
+            new Product(2, "Tweed & Faux Leather Tank", "Ribbed polo collar\nTwo-button placket\nLong sleeves with ribbed cuffs", 14990, "Tops",
                     new int[]{R.drawable.tweed_faux_leather_tank_1, R.drawable.tweed_faux_leather_tank_2, R.drawable.tweed_faux_leather_tank_3}),
-            new Product("Long-Sleeve Oxford Shirt", "Ribbed polo collar\nTwo-button placket\nLong sleeves with ribbed cuffs", 14990, "Tops",
+            new Product(3, "Long-Sleeve Oxford Shirt", "Ribbed polo collar\nTwo-button placket\nLong sleeves with ribbed cuffs", 14990, "Tops",
                     new int[]{R.drawable.long_sleeve_oxford_shirt}),
-            new Product("Crisscross-Back Maxi Dress", "Ribbed polo collar\nTwo-button placket\nLong sleeves with ribbed cuffs", 14990, "Gowns",
+            new Product(4, "Crisscross-Back Maxi Dress", "Ribbed polo collar\nTwo-button placket\nLong sleeves with ribbed cuffs", 14990, "Gowns",
                     new int[]{R.drawable.crisscross_back_maxi_dress_1, R.drawable.crisscross_back_maxi_dress_2}),
-            new Product("Faux-Suede Trim Henley Dress", "Ribbed polo collar\nTwo-button placket\nLong sleeves with ribbed cuffs", 14990, "Gowns",
+            new Product(5, "Faux-Suede Trim Henley Dress", "Ribbed polo collar\nTwo-button placket\nLong sleeves with ribbed cuffs", 14990, "Gowns",
                     new int[]{R.drawable.faux_suede_trim_henley_dress_1, R.drawable.faux_suede_trim_henley_dress_2}),
-            new Product("Cap-Sleeve Cutout-Neckline Sheath", "Ribbed polo collar\nTwo-button placket\nLong sleeves with ribbed cuffs", 14990, "Gowns",
+            new Product(6, "Cap-Sleeve Cutout-Neckline Sheath", "Ribbed polo collar\nTwo-button placket\nLong sleeves with ribbed cuffs", 14990, "Gowns",
                     new int[]{R.drawable.cap_sleeve_cutout_neckline_sheath_1, R.drawable.cap_sleeve_cutout_neckline_sheath_2})
     };
 
--- android_app/app/src/main/java/com/example/chi6rag/mykart/Product.java
+++ android_app/app/src/main/java/com/example/chi6rag/mykart/Product.java
@@ -1,15 +1,15 @@
 package com.example.chi6rag.mykart;
 
-import java.util.ArrayList;
-
 public class Product {
     public String name;
     public String description;
     public double price;
     public String category;
     public int[] imageResources;
+    public int id;
 
-    public Product(String name, String description, double price, String category, int[] imageResources) {
+    public Product(int id, String name, String description, double price, String category, int[] imageResources) {
+        this.id = id;
         this.name = name;
         this.description = description;
         this.price = price;
