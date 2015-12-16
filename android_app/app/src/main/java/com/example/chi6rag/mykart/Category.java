88714aa8d17cca9047d84fd12999989086039c84
prashant <prashantee@iitj.ac.in>
2015-12-16 16:31:32 +0530

    Adding Category

---
+++ android_app/app/src/main/java/com/example/chi6rag/mykart/Category.java
@@ -0,0 +1,11 @@
+package com.example.chi6rag.mykart;
+
+public class Category {
+    public final Integer id;
+    public final String name;
+
+    Category(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
