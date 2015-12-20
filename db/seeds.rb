45bdd0cdea0e7a14412f29e70cbff3dc0f8fb59c
Chirag Aggarwal <me@chi6rag.net>
2015-12-20 18:25:56 +0530

    Adding devise secret, production db configuration and precompilation to false

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
 Spree::Core::Engine.load_seed if defined?(Spree::Core)
 Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
