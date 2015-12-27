46afe27a8f81f12f7dc2d22da40b4a0fa38dfc38
Chirag Aggarwal <me@chi6rag.net>
2015-12-27 19:42:32 +0530

    Adding home button

--- android_app/app/src/main/java/com/example/chi6rag/mykart/MainActivity.java
+++ android_app/app/src/main/java/com/example/chi6rag/mykart/MainActivity.java
@@ -11,6 +11,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ExpandableListView;
@@ -111,6 +113,13 @@ public class MainActivity extends AppCompatActivity implements
                 return true;
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+        return true;
     }
 
     @Override
