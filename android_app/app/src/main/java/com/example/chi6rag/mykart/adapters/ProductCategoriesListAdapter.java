5d48f5da75b8cdeeb7a91bc9b762d9b0f9878664
Chirag Aggarwal <me@chi6rag.net>
2015-12-28 12:38:12 +0530

    Adding improved category list item

--- android_app/app/src/main/java/com/example/chi6rag/mykart/adapters/ProductCategoriesListAdapter.java
+++ android_app/app/src/main/java/com/example/chi6rag/mykart/adapters/ProductCategoriesListAdapter.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import com.example.chi6rag.mykart.R;
 import com.example.chi6rag.mykart.network.ProductCategory;
 
 import java.util.ArrayList;
@@ -41,11 +42,11 @@ public class ProductCategoriesListAdapter extends BaseAdapter {
         ProductCategory productCategory = getItem(position);
         View view;
         if (convertView == null) {
-            view = this.layoutInflator.inflate(android.R.layout.simple_list_item_1, parent, false);
+            view = this.layoutInflator.inflate(R.layout.product_category_list_item, parent, false);
         } else {
             view = convertView;
         }
-        TextView productCategoryName = (TextView) view.findViewById(android.R.id.text1);
+        TextView productCategoryName = (TextView) view.findViewById(R.id.product_category_list_item_text);
         productCategoryName.setText(productCategory.name);
         return view;
     }
